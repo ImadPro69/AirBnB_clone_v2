5bde2be058eb202eda93c64336821aa65e04dded
Imadeddine Es-sebaiy <125612764+ImadPro69@users.noreply.github.com>
2023-10-05 20:26:59 +0100

    Update 3-deploy_web_static.py

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -7,7 +7,7 @@ distributes an archive to the web servers
 from fabric.api import env, local, put, run
 from datetime import datetime
 from os.path import exists, isdir
-env.hosts = ['142.44.167.228', '144.217.246.195']
+env.hosts = ['52.3.247.55', '34.202.159.191']
 
 
 def do_pack():
