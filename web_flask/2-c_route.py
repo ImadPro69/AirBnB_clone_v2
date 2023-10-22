0986cb3f134c29f226f02a574202ab7032d76928
Imadeddine Es-sebaiy <125612764+ImadPro69@users.noreply.github.com>
2023-10-22 23:43:08 +0100

    Add files via upload

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,28 +1,37 @@
 #!/usr/bin/python3
-"""
-starts a Flask web application
-"""
-
+""" Script that starts a Flask web application """
 from flask import Flask
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def index():
-    """returns Hello HBNB!"""
+def hello_route():
+    """ Method that returns Hello HBNB! """
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """returns HBNB"""
+    """ Method that returns HBNB """
     return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def cisfun(text):
-    """display “C ” followed by the value of the text variable"""
-    return 'C ' + text.replace('_', ' ')
+def c_text(text):
+    """
+    This function takes a string argument 'text',
+    replaces all underscores with spaces,
+    and returns a string in the format 'C <text>'
+    when the /c/<text> route is accessed.
+    Args:
+        text (str): The text to be displayed after 'C '
+    Returns:
+        str: A string in the format 'C <text>'
+    """
+    text = text.replace('_', ' ')
+    return 'C {}'.format(text)
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+
+    app.run(host='0.0.0.0', port=5000)
--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,47 +1,75 @@
 #!/usr/bin/python3
-"""
-starts a Flask web application
-"""
-
+""" Script that starts a Flask web application """
 from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def index():
-    """returns Hello HBNB!"""
+def hello_route():
+    """ Method that returns Hello HBNB! """
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """returns HBNB"""
+    """ Method that returns HBNB """
     return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def cisfun(text):
-    """display “C ” followed by the value of the text variable"""
-    return 'C ' + text.replace('_', ' ')
+def c_text(text):
+    """
+    This function takes a string argument 'text',
+    replaces all underscores with spaces,
+    and returns a string in the format 'C <text>'
+    when the /c/<text> route is accessed.
+    Args:
+        text (str): The text to be displayed after 'C '
+    Returns:
+        str: A string in the format 'C <text>'
+    """
+    text = text.replace('_', ' ')
+    return 'C {}'.format(text)
 
 
-@app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def pythoniscool(text='is cool'):
-    """display “Python ”, followed by the value of the text variable"""
-    return 'Python ' + text.replace('_', ' ')
+@app.route('/python', strict_slashes=False)
+def python_text(text='is cool'):
+
+    """
+    This function takes a string argument 'text',
+    replaces all underscores with spaces,
+    and returns a string in the format 'Python <text>'
+    when the /python/<text> route is accessed.
+    Args:
+        text (str): The text to be displayed after 'Python '
+    Returns:
+        str: A string in the format 'Python <text>'
+    """
+    text = text.replace('_', ' ')
+    return 'Python {}'.format(text)
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
-def imanumber(n):
-    """display “n is a number” only if n is an integer"""
-    return "{:d} is a number".format(n)
+def number(n):
+    """
+    This function takes an integer argument 'n'
+     and returns a string in the format '<n> is a number'
+    when the /number/<n> route is accessed.
+    Args:
+        n (int): The number to be displayed
+    Returns:
+        str: A string in the format '<n> is a number'
+    """
+    return '{} is a number'.format(n)
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
-def numbersandtemplates(n):
-    """display a HTML page only if n is an integer"""
+def number_template(n):
+    """return render_template('number.html', n=n)"""
     return render_template('5-number.html', n=n)
 
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+
+    app.run(host='0.0.0.0', port=5000)
